a9a695651f85a5d8de454f8ac60152b1685a9f8c
LuizFelipe <luizfelipers19@gmail.com>
2021-05-29 23:58:44 -0300

    started to implement the search method by adding the baseUrl and finding the Search endpoint on spotify's docs

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,13 +1,22 @@
 import { clearToken, getToken } from 'utils'
 
 class SomosClient {
-  constructor() {}
+  constructor() {
+    this.accessToken = getToken()
+
+  }
 
   onError = error => {}
+
+ baseUrl = "	https://api.spotify.com/v1/"
 
   async getArtists() {
     // Obs: para chamadas na api, você já tem o token salvo no cookie, `authenticated_token` - use ele para mandar no header das chamadas - da uma olhada no `src/utils`
     // retornar a lista de artistas - https://developer.spotify.com/console/get-several-artists/
+
+    const busca = await axios.get(this.baseUrl)
+  
+
   }
 }
 
